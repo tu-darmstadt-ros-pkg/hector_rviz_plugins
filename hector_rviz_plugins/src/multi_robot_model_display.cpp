d840c66c609ba7f3a0874f5830c53563cb272255
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-03-26 12:39:49 +0100

    Back to the future. (Fixed year typo in copyright)

--- hector_rviz_plugins/src/multi_robot_model_display.cpp
+++ hector_rviz_plugins/src/multi_robot_model_display.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 20245 Stefan Fabian
+ * Copyright (C) 2025 Stefan Fabian
  *
  * This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
